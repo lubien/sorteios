1dfddbc45f1ecdc4f477d6b6765e0956dabae82b
lubien <lubien1996@gmail.com>
2022-10-15 14:22:41 -0300

    Install tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :sorteios, SorteiosWeb.Endpoint,
   secret_key_base: "cfFZYBbqRZKvoW5Lhi6uPMU/tUGutsyx3tIAgRR0f9AYvRxYVNIvpeq1IjHdn7+t",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
