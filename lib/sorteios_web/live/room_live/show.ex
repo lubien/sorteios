f73e094670ce05495101e92d17014139a3e99808
lubien <lubien1996@gmail.com>
2022-10-16 02:24:19 -0300

    Format

--- lib/sorteios_web/live/room_live/show.ex
+++ lib/sorteios_web/live/room_live/show.ex
@@ -16,7 +16,7 @@ defmodule SorteiosWeb.RoomLive.Show do
     }
 
     PubSub.subscribe(Sorteios.PubSub, topic)
-    {:ok, _} = Presence.track(self(), topic , email, current_user)
+    {:ok, _} = Presence.track(self(), topic, email, current_user)
 
     SorteiosWeb.Endpoint.subscribe(topic)
 
